a9e8451410e5a9c6c6db696d6f848d7665514cfd
Rutuvik Patil <you@example.com>
2023-05-27 11:51:55 +0530

    Mock5 Server Endpoints

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const json=require("json-server");
 
 const server=json.create();
+const router=json.router('db.json');
 const middleware=json.defaults();
-const router=json.router();
 
 server.use(middleware);
-server.use(router,'db.json');
-router("/api")
+server.use("",router);
 server.listen(8080,()=>{
     console.log("Running");
 })
