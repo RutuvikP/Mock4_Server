d2dbd36f2310beb25246a1419d7cc43e56b1742f
Rutuvik Patil <you@example.com>
2023-05-27 15:09:28 +0530

    Updated

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const jsonServer=require("json-server");
-const cors=require('cors');
 const server=jsonServer.create();
 const router=jsonServer.router('db.json');
 const middleware=jsonServer.defaults();
 
-server.use(cors());
+
 server.use(middleware);
-server.use(router);
+server.use("/api",router);
 server.listen(8080,()=>{
     console.log("Running");
 })
